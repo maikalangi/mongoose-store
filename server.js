767a0c4c905e721d1b93efb7ec104f32ae003c50
maikalangi <maikalangi@gmail.com>
2022-05-24 19:05:44 -0700

    sets up port listener

---
+++ server.js
@@ -0,0 +1,29 @@
+// DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+require('dotenv').config();
+
+// INITIALIZER
+const app = express();
+
+// CONFIGURATION
+const PORT = process.env.PORT;
+
+// INDEX
+
+// NEW
+
+// DELETE
+
+// UPDATE
+
+// CREATE
+
+// EDIT
+
+// SHOW
+
+
+// LISTENER
+app.listen(PORT, ()=>console.log(`server is listening on port: ${PORT}`));
